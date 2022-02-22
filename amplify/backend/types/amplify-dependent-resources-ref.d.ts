193306316c8325e59a680e62c25b754f3989ef59
apoorwa1 <apoorwa.saxena@gmail.com>
2022-02-22 22:56:30 +0000

    amplify blog app

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,20 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "MyBlogs": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    },
+    "auth": {
+        "MyBlogs": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
---
+++ src/ui-components/MyPostCollection.jsx
@@ -0,0 +1,45 @@
+/***************************************************************************
+ * The contents of this file were generated with Amplify Studio.           *
+ * Please refrain from making any modifications to this file.              *
+ * Any changes to this file will be overwritten when running amplify pull. *
+ **************************************************************************/
+
+/* eslint-disable */
+import React from "react";
+import { Post } from "../models";
+import {
+  getOverrideProps,
+  useDataStoreBinding,
+} from "@aws-amplify/ui-react/internal";
+import SocialA from "./SocialA";
+import { Collection } from "@aws-amplify/ui-react";
+export default function MyPostCollection(props) {
+  const { post, items: itemsProp, overrides: overridesProp, ...rest } = props;
+  const overrides = { ...overridesProp };
+  const items =
+    itemsProp !== undefined
+      ? itemsProp
+      : useDataStoreBinding({
+          type: "collection",
+          model: Post,
+        }).items;
+  return (
+    <Collection
+      type="list"
+      direction="column"
+      justifyContent="stretch"
+      items={items || []}
+      {...rest}
+      {...getOverrideProps(overrides, "Collection")}
+    >
+      {(item, index) => (
+        <SocialA
+          post={item}
+          margin="0 0 4px 0"
+          key={item.id}
+          {...getOverrideProps(overrides, "Collection.SocialA[0]")}
+        ></SocialA>
+      )}
+    </Collection>
+  );
+}
